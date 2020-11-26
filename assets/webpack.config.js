e882ced0d29a57305e7b8a20176b92e404e814d5
elixirprogrammer <boilercoding@gmail.com>
2020-11-26 02:16:35 -0500

    adds bootstrap setup

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -41,7 +41,18 @@ module.exports = (env, options) => {
             'css-loader',
             'sass-loader',
           ],
-        }
+        },
+        // Load fonts
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '../fonts'
+            },
+          }],
+        },
       ]
     },
     plugins: [
