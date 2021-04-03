3ae643a386305c3fccda5e55ece0d9e17fa9ccb5
elixirprogrammer <boilercoding@gmail.com>
2021-04-03 04:33:56 -0400

    adds user settings password change

--- lib/instagram_web/live/user_live/user_settings_pass_live.ex
+++ lib/instagram_web/live/user_live/user_settings_pass_live.ex
@@ -3,14 +3,33 @@ defmodule InstagramWeb.UserSettingsPassLive do
 
   alias Instagram.Accounts
   alias Instagram.Accounts.User
+  alias Instagram.Uploaders.Avatar
 
   @impl true
   def mount(_params, session, socket) do
     socket = assign_defaults(session, socket)
+    user = socket.assigns.current_user
 
     {:ok,
       socket
-      |> assign(:page_title, "Change Password")}
+      |> assign(:page_title, "Change Password")
+      |> assign(changeset: Accounts.change_user_password(user))}
+  end
+
+  @impl true
+  def handle_event("save", %{"user" => params}, socket) do
+    password = Map.get(params, "current_password")
+    user_params = Map.delete(params, "current_password")
+    case Accounts.update_user_password(socket.assigns.current_user, password, user_params) do
+      {:ok, _user} ->
+        {:noreply,
+          socket
+          |> put_flash(:info, "Password updated successfully.")
+          |> push_redirect(to: Routes.live_path(socket, InstagramWeb.UserSettingsPassLive))}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
   end
 
   @impl true
--- lib/instagram_web/router.ex
+++ lib/instagram_web/router.ex
@@ -55,10 +55,6 @@ defmodule InstagramWeb.Router do
     post "/users/register", UserRegistrationController, :create
     get "/users/log_in", UserSessionController, :new
     post "/users/log_in", UserSessionController, :create
-    get "/users/reset_password", UserResetPasswordController, :new
-    post "/users/reset_password", UserResetPasswordController, :create
-    get "/users/reset_password/:token", UserResetPasswordController, :edit
-    put "/users/reset_password/:token", UserResetPasswordController, :update
   end
 
   scope "/", InstagramWeb do
@@ -79,5 +75,9 @@ defmodule InstagramWeb.Router do
     get "/users/confirm", UserConfirmationController, :new
     post "/users/confirm", UserConfirmationController, :create
     get "/users/confirm/:token", UserConfirmationController, :confirm
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
   end
 end
